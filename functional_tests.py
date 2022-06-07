f592c4a83675c005befd1101c1a25eb3edca5d03
Nicholas Martinez <nedwardmartinez@gmail.com>
2022-06-07 17:19:30 -0500

    implemented mock token; send_keys race condition debugged with sleep; testing item addition

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
@@ -39,20 +39,33 @@ class NewVisitorTest(unittest.TestCase):
 
         input_box.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(2)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in the table",
-        )
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # A textbox persists to invite the addition of items. User enters "Use peacock feathers to make a fly"
 
-        self.fail("Finish the test!")
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        time.sleep(2)
  
         # The page updates again and shows both items on the list
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
         # User notices a unique URL and text that indicates the list will be saved
+
+        self.fail('Finish the test!')
 
         # The User visits the page to confirm the list is still there
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,42 @@
+from unittest import mock
+
 from django.template.loader import render_to_string
+from django.shortcuts import render
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
 
+@mock.patch('django.template.context_processors.get_token', mock.Mock(return_value='test_token'))
 class HomePageTest(TestCase):
+    '''Lists app unit testing class'''
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_root(self):
+        
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_html(self):
+
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render(request, 'home.html')
+
+        self.assertEqual(response.content.decode(), expected_html.content.decode())
+
+    def test_POST_request(self):
+        
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST['item_text'] = "1: Buy peacock feathers"
+        
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        
+        context = {'new_item_text': '1: Buy peacock feathers'}
+        expected_html = render(request, 'home.html', context)
+
+        self.assertEqual(response.content.decode(), expected_html.content.decode()) # FIXME content != expected_html
 
 # Create your tests here.
