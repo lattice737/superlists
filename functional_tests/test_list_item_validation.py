45557db6a99ff869aa5a6a45b3196fc9dc56be1c
Nicholas Martinez <nedwardmartinez@gmail.com>
2022-07-15 14:40:30 -0500

    change item validation test method

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,7 +5,7 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
-    def test_cannot_add_empty_list_items(self):
+    def test_empty_items_not_added(self):
 
         # User goes to home page and accidentally tries to submit an empty list item
         
@@ -51,3 +51,25 @@ class ItemValidationTest(FunctionalTest):
 
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_duplicate_items_not_added(self):
+
+        # User goes to home page and starts a new list
+
+        self.browser.get(self.live_server_url)
+
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally enters a duplicate item
+
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # An error message is displayed
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "This item is already on the list",
+        ))
